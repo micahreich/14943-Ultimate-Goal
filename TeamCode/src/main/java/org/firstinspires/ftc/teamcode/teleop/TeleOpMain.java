8a77ecc2efcbdba6ce896a494bd8d6d39e2a8bfb
Micah Reich <micahreich02@gmail.com>
2021-10-12 19:02:31 -0400

    Update TeleOpMain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.java
@@ -30,13 +30,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
-/**
- * This is a simple teleop routine for testing localization. Drive the robot around like a normal
- * teleop routine and make sure the robot's estimated pose matches the robot's actual pose (slight
- * errors are not out of the ordinary, especially with sudden drive motions). The goal of this
- * exercise is to ascertain whether the localizer has been configured properly (note: the pure
- * encoder localizer heading may be significantly off if the track width has not been tuned).
- */
+// Main teleop program
+
 @TeleOp(name="TeleOp Main", group = "teleop")
 public class TeleOpMain extends LinearOpMode {
     // hardware objects
